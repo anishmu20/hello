b46cfc6db92edc4abb2a9255d2784ecf266f9347
Anish <anishmu302@gmail.com>
2024-09-25 19:01:51 +0530

    second commit

---
+++ src/learn.java
@@ -0,0 +1,12 @@
+public class learn {
+
+    public static void main(String[] args) {
+        int res=sum(20,28);
+        System.out.println(+res);
+
+    }
+
+    static  int sum(int a,int b){
+        return a+b;
+    }
+}
